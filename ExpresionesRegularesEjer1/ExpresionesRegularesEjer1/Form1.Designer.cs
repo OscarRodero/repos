38302428c54c00999dc78087afcc67cacb9e985a
Óscar_Rodero <oscarrodero@hotmail.com>
2022-10-10 11:12:59 +0200

    Ejercicios de Expresiones Regulares añadidos.

--- ExpresionesRegularesEjer1/ExpresionesRegularesEjer1/Form1.Designer.cs
+++ ExpresionesRegularesEjer1/ExpresionesRegularesEjer1/Form1.Designer.cs
@@ -29,11 +29,291 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.PB1 = new System.Windows.Forms.PictureBox();
+            this.ListaImagenes = new System.Windows.Forms.ImageList(this.components);
+            this.txtBox1 = new System.Windows.Forms.TextBox();
+            this.btnValidar1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnValidar2 = new System.Windows.Forms.Button();
+            this.btnValidar3 = new System.Windows.Forms.Button();
+            this.btnValidar4 = new System.Windows.Forms.Button();
+            this.btnValidar5 = new System.Windows.Forms.Button();
+            this.btnValidar6 = new System.Windows.Forms.Button();
+            this.btnValidar7 = new System.Windows.Forms.Button();
+            this.btnValidar8 = new System.Windows.Forms.Button();
+            this.btnValidar9 = new System.Windows.Forms.Button();
+            this.btnValidar10 = new System.Windows.Forms.Button();
+            this.btnValidar11 = new System.Windows.Forms.Button();
+            this.btnValidar12 = new System.Windows.Forms.Button();
+            this.btnValidar13 = new System.Windows.Forms.Button();
+            this.btnValidar14 = new System.Windows.Forms.Button();
+            this.btnValidar15 = new System.Windows.Forms.Button();
+            this.btnValidar16 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.PB1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(32, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(73, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Validación 1:";
+            // 
+            // PB1
+            // 
+            this.PB1.Image = global::ExpresionesRegularesEjer1.Properties.Resources.png_clipart_check_mark_computer_icons_button_angle_leaf;
+            this.PB1.InitialImage = global::ExpresionesRegularesEjer1.Properties.Resources.png_clipart_check_mark_computer_icons_button_angle_leaf;
+            this.PB1.Location = new System.Drawing.Point(600, 24);
+            this.PB1.Name = "PB1";
+            this.PB1.Size = new System.Drawing.Size(172, 136);
+            this.PB1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.PB1.TabIndex = 1;
+            this.PB1.TabStop = false;
+            // 
+            // ListaImagenes
+            // 
+            this.ListaImagenes.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit;
+            this.ListaImagenes.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("ListaImagenes.ImageStream")));
+            this.ListaImagenes.TransparentColor = System.Drawing.Color.Transparent;
+            this.ListaImagenes.Images.SetKeyName(0, "png-clipart-check-mark-computer-icons-button-angle-leaf.png");
+            this.ListaImagenes.Images.SetKeyName(1, "png-transparent-red-x-logo-jingjinji-check-mark-error-red-error-angle-color-brush" +
+        ".png");
+            // 
+            // txtBox1
+            // 
+            this.txtBox1.Location = new System.Drawing.Point(32, 88);
+            this.txtBox1.Name = "txtBox1";
+            this.txtBox1.Size = new System.Drawing.Size(128, 23);
+            this.txtBox1.TabIndex = 2;
+            // 
+            // btnValidar1
+            // 
+            this.btnValidar1.Location = new System.Drawing.Point(176, 88);
+            this.btnValidar1.Name = "btnValidar1";
+            this.btnValidar1.Size = new System.Drawing.Size(75, 23);
+            this.btnValidar1.TabIndex = 3;
+            this.btnValidar1.Text = "Validar";
+            this.btnValidar1.UseVisualStyleBackColor = true;
+            this.btnValidar1.Click += new System.EventHandler(this.btnValidar1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(32, 64);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(424, 15);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Ingrese un número de 3 dígitos enteros, el carácter punto y 2 dígitos decimales.";
+            // 
+            // btnValidar2
+            // 
+            this.btnValidar2.Location = new System.Drawing.Point(176, 128);
+            this.btnValidar2.Name = "btnValidar2";
+            this.btnValidar2.Size = new System.Drawing.Size(75, 23);
+            this.btnValidar2.TabIndex = 5;
+            this.btnValidar2.Text = "Validar2";
+            this.btnValidar2.UseVisualStyleBackColor = true;
+            this.btnValidar2.Click += new System.EventHandler(this.btnValidar2_Click);
+            // 
+            // btnValidar3
+            // 
+            this.btnValidar3.Location = new System.Drawing.Point(176, 168);
+            this.btnValidar3.Name = "btnValidar3";
+            this.btnValidar3.Size = new System.Drawing.Size(75, 23);
+            this.btnValidar3.TabIndex = 6;
+            this.btnValidar3.Text = "Validar3";
+            this.btnValidar3.UseVisualStyleBackColor = true;
+            this.btnValidar3.Click += new System.EventHandler(this.btnValidar3_Click);
+            // 
+            // btnValidar4
+            // 
+            this.btnValidar4.Location = new System.Drawing.Point(176, 208);
+            this.btnValidar4.Name = "btnValidar4";
+            this.btnValidar4.Size = new System.Drawing.Size(75, 23);
+            this.btnValidar4.TabIndex = 7;
+            this.btnValidar4.Text = "Validar4";
+            this.btnValidar4.UseVisualStyleBackColor = true;
+            this.btnValidar4.Click += new System.EventHandler(this.btnValidar4_Click);
+            // 
+            // btnValidar5
+            // 
+            this.btnValidar5.Location = new System.Drawing.Point(176, 248);
+            this.btnValidar5.Name = "btnValidar5";
+            this.btnValidar5.Size = new System.Drawing.Size(75, 23);
+            this.btnValidar5.TabIndex = 8;
+            this.btnValidar5.Text = "Validar5";
+            this.btnValidar5.UseVisualStyleBackColor = true;
+            this.btnValidar5.Click += new System.EventHandler(this.btnValidar5_Click);
+            // 
+            // btnValidar6
+            // 
+            this.btnValidar6.Location = new System.Drawing.Point(176, 288);
+            this.btnValidar6.Name = "btnValidar6";
+            this.btnValidar6.Size = new System.Drawing.Size(75, 23);
+            this.btnValidar6.TabIndex = 9;
+            this.btnValidar6.Text = "Validar6";
+            this.btnValidar6.UseVisualStyleBackColor = true;
+            this.btnValidar6.Click += new System.EventHandler(this.btnValidar6_Click);
+            // 
+            // btnValidar7
+            // 
+            this.btnValidar7.Location = new System.Drawing.Point(264, 88);
+            this.btnValidar7.Name = "btnValidar7";
+            this.btnValidar7.Size = new System.Drawing.Size(75, 23);
+            this.btnValidar7.TabIndex = 10;
+            this.btnValidar7.Text = "Validar7";
+            this.btnValidar7.UseVisualStyleBackColor = true;
+            this.btnValidar7.Click += new System.EventHandler(this.btnValidar7_Click);
+            // 
+            // btnValidar8
+            // 
+            this.btnValidar8.Location = new System.Drawing.Point(264, 128);
+            this.btnValidar8.Name = "btnValidar8";
+            this.btnValidar8.Size = new System.Drawing.Size(75, 23);
+            this.btnValidar8.TabIndex = 11;
+            this.btnValidar8.Text = "Validar8";
+            this.btnValidar8.UseVisualStyleBackColor = true;
+            this.btnValidar8.Click += new System.EventHandler(this.btnValidar8_Click);
+            // 
+            // btnValidar9
+            // 
+            this.btnValidar9.Location = new System.Drawing.Point(264, 168);
+            this.btnValidar9.Name = "btnValidar9";
+            this.btnValidar9.Size = new System.Drawing.Size(75, 23);
+            this.btnValidar9.TabIndex = 12;
+            this.btnValidar9.Text = "Validar9";
+            this.btnValidar9.UseVisualStyleBackColor = true;
+            this.btnValidar9.Click += new System.EventHandler(this.btnValidar9_Click);
+            // 
+            // btnValidar10
+            // 
+            this.btnValidar10.Location = new System.Drawing.Point(264, 208);
+            this.btnValidar10.Name = "btnValidar10";
+            this.btnValidar10.Size = new System.Drawing.Size(75, 23);
+            this.btnValidar10.TabIndex = 13;
+            this.btnValidar10.Text = "Validar10";
+            this.btnValidar10.UseVisualStyleBackColor = true;
+            this.btnValidar10.Click += new System.EventHandler(this.btnValidar10_Click);
+            // 
+            // btnValidar11
+            // 
+            this.btnValidar11.Location = new System.Drawing.Point(264, 248);
+            this.btnValidar11.Name = "btnValidar11";
+            this.btnValidar11.Size = new System.Drawing.Size(75, 23);
+            this.btnValidar11.TabIndex = 14;
+            this.btnValidar11.Text = "Validar11";
+            this.btnValidar11.UseVisualStyleBackColor = true;
+            this.btnValidar11.Click += new System.EventHandler(this.btnValidar11_Click);
+            // 
+            // btnValidar12
+            // 
+            this.btnValidar12.Location = new System.Drawing.Point(264, 288);
+            this.btnValidar12.Name = "btnValidar12";
+            this.btnValidar12.Size = new System.Drawing.Size(75, 23);
+            this.btnValidar12.TabIndex = 15;
+            this.btnValidar12.Text = "Validar12";
+            this.btnValidar12.UseVisualStyleBackColor = true;
+            this.btnValidar12.Click += new System.EventHandler(this.btnValidar12_Click);
+            // 
+            // btnValidar13
+            // 
+            this.btnValidar13.Location = new System.Drawing.Point(352, 88);
+            this.btnValidar13.Name = "btnValidar13";
+            this.btnValidar13.Size = new System.Drawing.Size(75, 23);
+            this.btnValidar13.TabIndex = 16;
+            this.btnValidar13.Text = "Validar13";
+            this.btnValidar13.UseVisualStyleBackColor = true;
+            this.btnValidar13.Click += new System.EventHandler(this.btnValidar13_Click);
+            // 
+            // btnValidar14
+            // 
+            this.btnValidar14.Location = new System.Drawing.Point(352, 128);
+            this.btnValidar14.Name = "btnValidar14";
+            this.btnValidar14.Size = new System.Drawing.Size(75, 23);
+            this.btnValidar14.TabIndex = 17;
+            this.btnValidar14.Text = "Validar14";
+            this.btnValidar14.UseVisualStyleBackColor = true;
+            this.btnValidar14.Click += new System.EventHandler(this.btnValidar14_Click);
+            // 
+            // btnValidar15
+            // 
+            this.btnValidar15.Location = new System.Drawing.Point(352, 168);
+            this.btnValidar15.Name = "btnValidar15";
+            this.btnValidar15.Size = new System.Drawing.Size(75, 23);
+            this.btnValidar15.TabIndex = 18;
+            this.btnValidar15.Text = "Validar15";
+            this.btnValidar15.UseVisualStyleBackColor = true;
+            this.btnValidar15.Click += new System.EventHandler(this.btnValidar15_Click);
+            // 
+            // btnValidar16
+            // 
+            this.btnValidar16.Location = new System.Drawing.Point(352, 208);
+            this.btnValidar16.Name = "btnValidar16";
+            this.btnValidar16.Size = new System.Drawing.Size(75, 23);
+            this.btnValidar16.TabIndex = 19;
+            this.btnValidar16.Text = "Validar16";
+            this.btnValidar16.UseVisualStyleBackColor = true;
+            this.btnValidar16.Click += new System.EventHandler(this.btnValidar16_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnValidar16);
+            this.Controls.Add(this.btnValidar15);
+            this.Controls.Add(this.btnValidar14);
+            this.Controls.Add(this.btnValidar13);
+            this.Controls.Add(this.btnValidar12);
+            this.Controls.Add(this.btnValidar11);
+            this.Controls.Add(this.btnValidar10);
+            this.Controls.Add(this.btnValidar9);
+            this.Controls.Add(this.btnValidar8);
+            this.Controls.Add(this.btnValidar7);
+            this.Controls.Add(this.btnValidar6);
+            this.Controls.Add(this.btnValidar5);
+            this.Controls.Add(this.btnValidar4);
+            this.Controls.Add(this.btnValidar3);
+            this.Controls.Add(this.btnValidar2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnValidar1);
+            this.Controls.Add(this.txtBox1);
+            this.Controls.Add(this.PB1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.PB1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private Label label1;
+        private PictureBox PB1;
+        private ImageList ListaImagenes;
+        private TextBox txtBox1;
+        private Button btnValidar1;
+        private Label label2;
+        private Button btnValidar2;
+        private Button btnValidar3;
+        private Button btnValidar4;
+        private Button btnValidar5;
+        private Button btnValidar6;
+        private Button btnValidar7;
+        private Button btnValidar8;
+        private Button btnValidar9;
+        private Button btnValidar10;
+        private Button btnValidar11;
+        private Button btnValidar12;
+        private Button btnValidar13;
+        private Button btnValidar14;
+        private Button btnValidar15;
+        private Button btnValidar16;
     }
 }
--- ExpresionesRegularesEjer1/ExpresionesRegularesEjer1/Form1.cs
+++ ExpresionesRegularesEjer1/ExpresionesRegularesEjer1/Form1.cs
@@ -1,10 +1,235 @@
+using System.Text.RegularExpressions;
+
 namespace ExpresionesRegularesEjer1
 {
     public partial class Form1 : Form
     {
+        private const string Patron = @"\A\d{3}\.\d{2}\Z";
+        private const string Patron2 = @"\A[(]\d{1,3}[,]\d{1,3}[)]\Z";
+        private const string Patron3 = @"\A\(d{3}[,]){3}\d{3}";
+        private const string Patron4 = @"\A(lunes|martes|miercoles|jueves|viernes|sabado|domingo)\s\d{1,2}\Z";
+        private const string Patron5 = @"\A([0-1]\d|2[0-3])[:][0-5]\d([:][0-5]\d)?\Z";
+        private const string Patron6 = @"\A(ES)\d{2}[-]\d{4}[-]\d{2}[-]\d{10}\Z";
+        private const string Patron7 = @"\A([-]?\d{1})\Z";
+        private const string Patron8 = @"\A(\d{8}[TRWAGMYFPDXBNJZSQVHLCKE])\Z";
+        private const string Patron9 = @"\A([0-255][.][0-255][.][0-255][.][0-255])\Z"; //NoFunciona
+        private const string Patron10 = @"\A\d\d\d\d(\s)?[A-Z][A-Z][A-Z]";
+        private const string Patron11 = @"\A([-]?\d{1}(.|,)\d)\Z";
+        private const string Patron12 = @"\A([0-3]?\d[/][0-1]?\d[/][1-2]\d\d\d)\Z";
+        private const string Patron13 = @"\A([A-Z][a-z]+(\s[A-Z][a-z]++)?)\Z";
+        private const string Patron14 = @"\A[@]\D+\Z";
+        private const string Patron15 = @"\A((987|979)[-]\d[-]\d{2}[-]\d{6}[-]\d)\Z";
+        private const string Patron16 = @"\A([A-Z][a-z]\d\w{15})\Z";
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void btnValidar1_Click(object sender, EventArgs e)
+        {
+            bool boo_resutlado = Regex.IsMatch(txtBox1.Text, Patron);
+            if (boo_resutlado)
+            {
+                PB1.Image = ListaImagenes.Images[0];
+            }
+            else
+            {
+                PB1.Image = ListaImagenes.Images[1];
+            }
+        }
+
+        private void btnValidar2_Click(object sender, EventArgs e)
+        {
+            bool boo_resutlado = Regex.IsMatch(txtBox1.Text, Patron2);
+            if (boo_resutlado)
+            {
+                PB1.Image = ListaImagenes.Images[0];
+            }
+            else
+            {
+                PB1.Image = ListaImagenes.Images[1];
+            }
+        }
+
+        private void btnValidar3_Click(object sender, EventArgs e)
+        {
+            bool boo_resutlado = Regex.IsMatch(txtBox1.Text, Patron3);
+            if (boo_resutlado)
+            {
+                PB1.Image = ListaImagenes.Images[0];
+            }
+            else
+            {
+                PB1.Image = ListaImagenes.Images[1];
+            }
+        }
+        private void btnValidar4_Click(object sender, EventArgs e)
+        {
+            bool boo_resutlado = Regex.IsMatch(txtBox1.Text, Patron4);
+            if (boo_resutlado)
+            {
+                PB1.Image = ListaImagenes.Images[0];
+            }
+            else
+            {
+                PB1.Image = ListaImagenes.Images[1];
+            }
+        }
+        private void btnValidar5_Click(object sender, EventArgs e)
+        {
+            bool boo_resutlado = Regex.IsMatch(txtBox1.Text, Patron5);
+            if (boo_resutlado)
+            {
+                PB1.Image = ListaImagenes.Images[0];
+            }
+            else
+            {
+                PB1.Image = ListaImagenes.Images[1];
+            }
+        }
+
+        private void btnValidar6_Click(object sender, EventArgs e)
+        {
+            bool boo_resutlado = Regex.IsMatch(txtBox1.Text, Patron6);
+            if (boo_resutlado)
+            {
+                PB1.Image = ListaImagenes.Images[0];
+            }
+            else
+            {
+                PB1.Image = ListaImagenes.Images[1];
+            }
+        }
+
+        private void btnValidar7_Click(object sender, EventArgs e)
+        {
+            bool boo_resutlado = Regex.IsMatch(txtBox1.Text, Patron7);
+            if (boo_resutlado)
+            {
+                PB1.Image = ListaImagenes.Images[0];
+            }
+            else
+            {
+                PB1.Image = ListaImagenes.Images[1];
+            }
+        }
+
+        private void btnValidar8_Click(object sender, EventArgs e)
+        {
+            bool boo_resutlado = Regex.IsMatch(txtBox1.Text, Patron8);
+            if (boo_resutlado)
+            {
+                PB1.Image = ListaImagenes.Images[0];
+            }
+            else
+            {
+                PB1.Image = ListaImagenes.Images[1];
+            }
+        }
+
+        private void btnValidar9_Click(object sender, EventArgs e)
+        {
+            bool boo_resutlado = Regex.IsMatch(txtBox1.Text, Patron9);
+            if (boo_resutlado)
+            {
+                PB1.Image = ListaImagenes.Images[0];
+            }
+            else
+            {
+                PB1.Image = ListaImagenes.Images[1];
+            }
+        }
+
+        private void btnValidar10_Click(object sender, EventArgs e)
+        {
+            bool boo_resutlado = Regex.IsMatch(txtBox1.Text, Patron10);
+            if (boo_resutlado)
+            {
+                PB1.Image = ListaImagenes.Images[0];
+            }
+            else
+            {
+                PB1.Image = ListaImagenes.Images[1];
+            }
+        }
+
+        private void btnValidar11_Click(object sender, EventArgs e)
+        {
+            bool boo_resutlado = Regex.IsMatch(txtBox1.Text, Patron11);
+            if (boo_resutlado)
+            {
+                PB1.Image = ListaImagenes.Images[0];
+            }
+            else
+            {
+                PB1.Image = ListaImagenes.Images[1];
+            }
+        }
+
+        private void btnValidar12_Click(object sender, EventArgs e)
+        {
+            bool boo_resutlado = Regex.IsMatch(txtBox1.Text, Patron12);
+            if (boo_resutlado)
+            {
+                PB1.Image = ListaImagenes.Images[0];
+            }
+            else
+            {
+                PB1.Image = ListaImagenes.Images[1];
+            }
+        }
+
+        private void btnValidar13_Click(object sender, EventArgs e)
+        {
+            bool boo_resutlado = Regex.IsMatch(txtBox1.Text, Patron13);
+            if (boo_resutlado)
+            {
+                PB1.Image = ListaImagenes.Images[0];
+            }
+            else
+            {
+                PB1.Image = ListaImagenes.Images[1];
+            }
+        }
+
+        private void btnValidar14_Click(object sender, EventArgs e)
+        {
+            bool boo_resutlado = Regex.IsMatch(txtBox1.Text, Patron14);
+            if (boo_resutlado)
+            {
+                PB1.Image = ListaImagenes.Images[0];
+            }
+            else
+            {
+                PB1.Image = ListaImagenes.Images[1];
+            }
+        }
+
+        private void btnValidar15_Click(object sender, EventArgs e)
+        {
+            bool boo_resutlado = Regex.IsMatch(txtBox1.Text, Patron15);
+            if (boo_resutlado)
+            {
+                PB1.Image = ListaImagenes.Images[0];
+            }
+            else
+            {
+                PB1.Image = ListaImagenes.Images[1];
+            }
+        }
+
+        private void btnValidar16_Click(object sender, EventArgs e)
+        {
+            bool boo_resutlado = Regex.IsMatch(txtBox1.Text, Patron16);
+            if (boo_resutlado)
+            {
+                PB1.Image = ListaImagenes.Images[0];
+            }
+            else
+            {
+                PB1.Image = ListaImagenes.Images[1];
+            }
         }
     }
 }
